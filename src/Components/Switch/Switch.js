aa455d1fc4bcd740941158913647c958517c57f2
Jesus <galorasd@gmail.com>
2022-07-18 13:33:14 +0200

    Feat(Accesible-card): Add keyboard support to switches.

--- src/Components/Switch/Switch.js
+++ src/Components/Switch/Switch.js
@@ -8,10 +8,10 @@ const Switch = (props) => {
     <div className="switch">
         <div className="switch__label" id="switch-label" aria-hidden="true" data-id="text">{props.label}</div>
         <div className="switch__toogle-wrapper" onClick={() => setActive((prevActive) => !prevActive)}>
-          <div className="switch__toggle">{active}</div>
-          <div className="switch__inside-text" aria-hidden="true">{active ? 'ON' : 'OFF'}</div>
+          <div role="checkbox" aria-checked={active} aria-labelledby="switch-label" tabindex="0" className="switch__toggle">{active}</div>
+          <div className="switch__inside-text" >{active ? 'ON' : 'OFF'}</div>
         </div>  
-        <input className="switch__input" aria-labelledby="switch-label" role="switch" aria-checked={active} type="checkbox" />
+        <input className="switch__input" aria-labelledby="switch-label" tabindex="-1" aria-hidden="true" type="checkbox" />
     </div>
   );
 };
