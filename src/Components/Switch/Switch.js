724532a3de425553f2778ceb2cd314753bece012
Jesus <galorasd@gmail.com>
2022-07-19 12:24:15 +0200

    Fix(accesible-card) Improved keyboard support for users who use screen reader.

--- src/Components/Switch/Switch.js
+++ src/Components/Switch/Switch.js
@@ -9,13 +9,14 @@ const Switch = (props) => {
     <div className="switch">
         <div className="switch__label" id={props.id} aria-hidden="true" data-id="text">{props.label}</div>
         <div className={`switch__toogle-wrapper ${active ? 'checked' : null}`} onClick={() => setActive((prevActive) => !prevActive)}>
-          <div role="checkbox" aria-checked={active} aria-labelledby={props.id} className={`switch__toggle ${active ? 'checked' : null}`}>{active}</div>
+          <div tabindex="0" role="checkbox" aria-checked={active} aria-labelledby={props.id} className={`switch__toggle ${active ? 'checked' : null}`}>{active}</div>
           <div className={`switch__inside-text ${active ? 'checked' : null}`}>{active ? 'ON' : 'OFF'}</div>
           <input
           className="switch__input"
           aria-labelledby="switch-label"
           aria-hidden="true"
           type="checkbox"
+          tabindex="-1"
           checked={active}
           onChange={updateCheckbox} />
         </div>  
