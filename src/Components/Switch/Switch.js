d8cacb09fc8a5cb4b62e040107a6776d0ba04874
Ibermatica-Accesibilidad <j.delpozo@ibermatica.com>
2022-07-19 11:11:19 +0200

    chore(accessible-card): removes id prop and makes use of label to name id

--- src/Components/Switch/Switch.js
+++ src/Components/Switch/Switch.js
@@ -7,9 +7,9 @@ const Switch = (props) => {
 
   return (
     <div className="switch">
-        <div className="switch__label" id={props.id} aria-hidden="true" data-id="text">{props.label}</div>
+        <div className="switch__label" id={props.label} aria-hidden="true" data-id="text">{props.label}</div>
         <div className={`switch__toogle-wrapper ${active ? 'checked' : null}`} onClick={() => setActive((prevActive) => !prevActive)}>
-          <div role="checkbox" aria-checked={active} aria-labelledby={props.id} className={`switch__toggle ${active ? 'checked' : null}`}>{active}</div>
+          <div role="checkbox" aria-checked={active} aria-labelledby={props.label} className={`switch__toggle ${active ? 'checked' : null}`}>{active}</div>
           <div className={`switch__inside-text ${active ? 'checked' : null}`}>{active ? 'ON' : 'OFF'}</div>
           <input
           className="switch__input"
